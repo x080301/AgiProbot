59b980fcd62b11c4225767911e2d6bc2de773937
x080301 <fuhao_dsst@163.com>
2023-02-19 11:15:28 +0100

    nothing change

--- agi_project_10_category_reconstruct/testing_case.py
+++ agi_project_10_category_reconstruct/testing_case.py
@@ -168,20 +168,20 @@ def pipline_example():
 
 
 if __name__ == '__main__':
-    # print(test_load_pretrained_model(expected_return=True))
-    # print(test_load_pretrained_model(expected_return=False))
+    print(test_load_pretrained_model(expected_return=True))
+    print(test_load_pretrained_model(expected_return=False))
 
-    # test_load_data()
-    # test_prediction()
-    # test_find_screws_position(with_cover=True)
-    # test_find_screws_position(with_cover=False)
+    test_load_data()
+    test_prediction()
+    test_find_screws_position(with_cover=True)
+    test_find_screws_position(with_cover=False)
 
-    # test_find_screws_direction(with_cover=True)
-    # test_find_screws_direction(with_cover=False)
+    test_find_screws_direction(with_cover=True)
+    test_find_screws_direction(with_cover=False)
 
-    # test_find_gear_position(with_cover=True)
-    # test_find_gear_position(with_cover=False)
+    test_find_gear_position(with_cover=True)
+    test_find_gear_position(with_cover=False)
 
-    pipline_example()
+    # pipline_example()
 
     pass
