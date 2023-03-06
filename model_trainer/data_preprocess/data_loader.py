adc914dfa98eaf1472b675dbd69e8493bbc81250
x080301 <fuhao_dsst@163.com>
2023-03-06 11:54:00 +0100

    dataloader

--- model_trainer/data_preprocess/data_loader.py
+++ model_trainer/data_preprocess/data_loader.py
@@ -8,12 +8,41 @@ import torchvision as tv
 import os
 
 
+def data_preloading(data_pandas_csv):
+    """
+    data_preloading(data_pandas_csv)
+    read pcd data to make a csv file, which consists data direction and sampled index
+
+    Args:
+        data_pandas_csv (str): csv_direction
+
+    Returns:
+        pandas.core.frame.DataFrame
+    """
+    pass
+
+
 class MotorDataset(Dataset):
     def __init__(self, data_pandas_csv, mode, num_points=4096):
-        self.data = data_pandas_csv
+        """
+        MotorDataset(data_pandas_csv, mode, num_points=4096)
+        Dataloader
+
+        Args:
+            data_pandas_csv (str or pandas.core.frame.DataFrame): str, make csv file before taining;
+
+        Returns:
+            None
+        """
+
         self.mode = mode
         self.num_points = num_points
         self.classification_label_list = ['A0', 'A1', 'A2', 'B0']
+
+        if type(data_pandas_csv) is str:
+            self.data = data_preloading(data_pandas_csv)
+        else:
+            self.data = data_pandas_csv
 
     def __len__(self):
         return len(self.data)
@@ -26,16 +55,16 @@ class MotorDataset(Dataset):
         point_cloud_dir = self.data.iloc[index, 0]
 
         # ***********************************************************
-        # load points(N*3) and segementation_label(N) from .np file
+        # load points(N*3) and segmentation_label(N) from .np file
         # the order of the array should be shuffled and the array should be resampled to num_points(4096 default) points
 
         data_from_file = np.load(point_cloud_dir)
         point_cloud = data_from_file[:, 0:3]
-        segementation_label = data_from_file[:, 6]
+        segmentation_label = data_from_file[:, 6]
 
         choice = np.random.choice(point_cloud.shape[0], self.num_points, replace=True)
         point_cloud = point_cloud[choice, :]
-        segementation_label = segementation_label[choice]
+        segmentation_label = segmentation_label[choice]
         # ***********************************************************
 
         # ***********************************************************
@@ -52,4 +81,4 @@ class MotorDataset(Dataset):
         # TODO
         # goals?何意
 
-        return point_cloud, segementation_label, classification_label
+        return point_cloud, segmentation_label, classification_label
