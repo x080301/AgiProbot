6f3c9370eecc8d18894eda40e04bdf3657030671
x080301 <fuhao_dsst@163.com>
2023-05-28 03:15:55 +0200

    train line

--- binary_segmentation/train_line.py
+++ binary_segmentation/train_line.py
@@ -1,18 +1,20 @@
 import os
 
-file_list = []
+if __name__ == "__main__":
 
-keep_training = True
-while keep_training:
-    keep_training = False
+    file_list = []
 
-    for txt_file_name in os.listdir('train_line'):
-        if txt_file_name not in file_list:
-            file_list.append(txt_file_name)
-            keep_training = True
-            with open('train_line' + '/' + txt_file_name) as f:
-                executable_str = f.read()
-            try:
-                exec(executable_str)
-            except Exception:
-                pass
+    keep_training = True
+    while keep_training:
+        keep_training = False
+
+        for txt_file_name in os.listdir('train_line'):
+            if txt_file_name not in file_list:
+                file_list.append(txt_file_name)
+                keep_training = True
+                with open('train_line' + '/' + txt_file_name) as f:
+                    executable_str = f.read()
+                try:
+                    exec(executable_str)
+                except Exception:
+                    pass
