ab843a900fc3332e016e39a5dd992b71bd14eb4d
x080301 <fuhao_dsst@163.com>
2023-07-11 09:01:21 +0200

    debug

--- binary_segmentation/train.py
+++ binary_segmentation/train.py
@@ -14,7 +14,10 @@ from train_and_test.dpp_trainer import BinarySegmentationDPP
 
 train_txt = None
 if __name__ == "__main__":
-    config_dir = 'config/binary_segmentation_zivid_5_7.yaml'
+    config_dir = 'config/binary_segmentation_lr_minus_4_minus_6.yaml'
     print(config_dir)
     bsdpp = BinarySegmentationDPP(train_txt, config_dir)
     bsdpp.train_dpp()
+
+
+
