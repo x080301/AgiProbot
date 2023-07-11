4e8421a2815fa0c96a2a96520c688287311ac097
x080301 <fuhao_dsst@163.com>
2023-07-11 10:27:39 +0200

    hot update

--- binary_segmentation/train.py
+++ binary_segmentation/train.py
@@ -8,16 +8,20 @@
 # binarysegmentation.train()
 
 # from train_and_test.dpp_trainer import BinarySegmentationDPP
-from train_and_test.dpp_trainer_freeze import BinarySegmentationDPP
 
-from train_and_test.dpp_trainer import BinarySegmentationDPP
+from train_and_test.pre_train_fine_tune import BinarySegmentationDPP
+import shutil
 
 train_txt = None
 if __name__ == "__main__":
-    config_dir = 'config/binary_segmentation_lr_minus_4_minus_6.yaml'
+    config_dir = 'config/binary_segmentation_5_6.yaml'
+    print(config_dir)
+    bsdpp = BinarySegmentationDPP(train_txt, config_dir)
+    best_m_path = bsdpp.train_dpp()
+
+    shutil.copyfile(best_m_path, 'best_m.pth')
+
+    config_dir = 'config/binary_segmentation_fine_tune_6_8.yaml'
     print(config_dir)
     bsdpp = BinarySegmentationDPP(train_txt, config_dir)
     bsdpp.train_dpp()
-
-
-
