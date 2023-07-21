939d324f5f3c3bbc1f1b3133301742676876e6dd
x080301 <fuhao_dsst@163.com>
2023-07-21 19:12:09 +0200

    new train

--- binary_segmentation/utilities/tensorboardreader.py
+++ binary_segmentation/utilities/tensorboardreader.py
@@ -29,10 +29,15 @@ train_case_dic = {'2023_05_11_13_09_2048': '2048', '2023_05_10_06_00_4096': '409
                   '2023_06_02_09_56_zivid_freeze': 'zivid_5_8_freeze',
                   '2023_07_12_21_49_4_7_no_pretrain': 'zivid_4_7_no_pretrain',
                   '2023_07_15_20_02_no_rotation_augument': 'lr 1e-4->-6 no_rotatoion_augument',
-                  '2023_07_16_04_46': 'pretrain 1e-5->1e-7',
-                  '2023_07_16_13_33': 'fine tune 5e-7->1e-8'
+                  '2023_07_16_04_46_pretrain_1e51e7': 'pretrain 1e-5->1e-7',
+                  '2023_07_16_13_33_fine_tune_5e71e8': 'fine tune 5e-7->1e-8',
+                  '2023_07_17_08_19_pretain_5_6': 'pretrain 1e-5->1e-6',
+                  '2023_07_17_17_06_fine_tune_6_8': 'fine tune 5e->6->1e-8',
+                  '2023_07_20_05_52_100_epoch_pretain_4_5': 'pretrain 1e-4->1e-5',
+                  '2023_07_20_10_16_100_epoch_fine_tune_5_7': 'fine tune 5e->1e-7'
                   }
-show_list = ['pretrain 1e-5->1e-7', 'fine tune 5e-7->1e-8']
+show_list = ['lr 1e-4->-6', 'zivid_5_8', 'zivid_4_7_no_pretrain', 'pretrain 1e-5->1e-7', 'fine tune 5e-7->1e-8',
+             'pretrain 1e-5->1e-6', 'fine tune 5e->6->1e-8', 'pretrain 1e-4->1e-5', 'fine tune 5e->1e-7']
 plt.title("pretain & fine tune")
 
 mIoU_dict = {}
@@ -55,6 +60,8 @@ for key in show_list:
     '''if key == 'pre_trained':
         continue'''
     x = mIoU_dict[key][0]
+    print(x)
+    print(type(x))
     y = mIoU_dict[key][1]
     plt.plot(x, y, color=(0.9, 0.9, 0.9))
     y_max = max(max(y), y_max)
--- blender/generate_syn_dataset_with_blender.py
+++ blender/generate_syn_dataset_with_blender.py
@@ -5,6 +5,7 @@ import open3d as o3d
 import numpy as np
 import scipy
 from tqdm import tqdm
+import torch
 
 # This color assignment is unified in project agiprobot.
 rgb_dic = {'Void': [207, 207, 207],
@@ -39,6 +40,26 @@ def read_one_mesh(file_dir=r"C:\Users\Lenovo\Desktop\Alignment\Alignment\Motor_0
     return pcd
 
 
+def set_points_colors_torch(raw_points_pcd, all_parts_pcd):
+    raw_points = torch.tensor(np.asarray(raw_points_pcd.points)).cuda(non_blocking=True)
+    all_parts_pcd_points = torch.tensor(np.asarray(all_parts_pcd.points)).cuda(non_blocking=True)
+    all_parts_pcd_colors = torch.tensor(np.asarray(all_parts_pcd.colors)).cuda(non_blocking=True)
+
+    # set raw_points_pcd color according to the nearest point in all parts pcd
+
+    raw_points_colors = torch.empty((0, 3))
+    for i in tqdm(range(0, raw_points.shape[0], 1000)):
+        get_manhattan_distance = torch.nn.PairwiseDistance(p=1)
+        manhattan_distance = get_manhattan_distance(raw_points[i:i + 1000, :], all_parts_pcd_points)
+        _, closest_point_index = manhattan_distance.topk(k=1, dim=-1)
+
+        raw_points_colors = np.concatenate((raw_points_colors, all_parts_pcd_colors[closest_point_index, :]), axis=0)
+
+    raw_points_pcd.colors = o3d.utility.Vector3dVector(raw_points_colors)
+
+    return raw_points_pcd
+
+
 def set_points_colors(raw_points_pcd, all_parts_pcd):
     raw_points = np.asarray(raw_points_pcd.points)
     all_parts_pcd_points = np.asarray(all_parts_pcd.points)
@@ -61,88 +82,98 @@ def set_points_colors(raw_points_pcd, all_parts_pcd):
 def generate_pcd_with_label(source_dir, save_dir):
     """
     generate pcd files from blender models
+    !!!update cmd_str before use!!! /softwares/blender/ should be redirected to your own blender.exe direction!
 
     :param source_dir: directory where blender models are located as .blend files
     :param save_dir: directory where to save the results as .pcd files
     :return: no return
     """
+    generated_pcd_list = os.listdir(save_dir)
+
     for root, _, files in os.walk(source_dir):
         for file_name in files:
-            if 'Szene.blend' in file_name:
+            if 'Szene.blend' not in file_name:
+                continue
 
-                # ******************* #
-                # raw point cloud without label
-                # ******************* #
-                # copy source to intermediate results
-                shutil.copyfile(os.path.join(root, file_name),
-                                'D:/softwares/blender/Intermediate_results/blender_scene.blend')
+            save_path = root.split('/')[-1].split('\\')[0] + '_' + root.split('/')[-1].split('\\')[1] + '.pcd'
+            if save_path in generated_pcd_list:
+                continue
 
-                # run python script in blender with Windows CMD to generate an .obj file,
-                # which contains the synthetic large motor model without label.
-                # python script: pyscript_in_blender.py
-                site = {"source_blender": "Intermediate_results/blender_scene.blend",
-                        "py_script": "D:/Jupyter/AgiProbot/large_motor_segmentation/blender/blender_scripts/" +
-                                     "pyscript_in_blender.py"}
-                cmd_str = 'D: & ' \
-                          'cd /softwares/blender/ & ' \
-                          'blender.exe --background {source_blender} --python {py_script}'.format(**site)
-                subprocess.run(cmd_str, shell=True)
+            # ******************* #
+            # raw point cloud without label
+            # ******************* #
+            # copy source to intermediate results
+            shutil.copyfile(os.path.join(root, file_name),
+                            'D:/softwares/blender/Intermediate_results/blender_scene.blend')
 
-                # load obj and sample it to point cloud
-                print('sampling')
-                whole_model_pcd = read_one_mesh(
-                    'D:/softwares/blender/Intermediate_results/intermediate_obj.obj',
-                    number_of_points=200000
-                )
+            # run python script in blender with Windows CMD to generate an .obj file,
+            # which contains the synthetic large motor model without label.
+            # python script: pyscript_in_blender.py
+            site = {"source_blender": "Intermediate_results/blender_scene.blend",
+                    "py_script": os.path.dirname(__file__) + "/blender_scripts/" +
+                                 "pyscript_in_blender.py"}
+            cmd_str = 'D: & ' \
+                      'cd /softwares/blender/ & ' \
+                      'blender.exe --background {source_blender} --python {py_script}'.format(**site)
+            subprocess.run(cmd_str, shell=True)
 
-                # ******************* #
-                # points in parts with label
-                # ******************* #
-                # run python script in blender to generate a series of .obj files.
-                # Each .obj file contains objects of one semantic type.
-                # python script: generate_obj_with_label_using_blender.py
-                site = {
-                    "source_blender": "Intermediate_results/blender_scene.blend",
-                    "py_script": 'D:/Jupyter/AgiProbot/large_motor_segmentation/blender/blender_scripts/' +
-                                 'generate_obj_with_label_using_blender.py'
-                }
-                cmd_str = 'D: & ' \
-                          'cd /softwares/blender/ & ' \
-                          'blender.exe --background {source_blender} --python {py_script}'.format(**site)
-                subprocess.run(cmd_str, shell=True)
+            # load obj and sample it to point cloud
+            print('sampling')
+            whole_model_pcd = read_one_mesh(
+                'D:/softwares/blender/Intermediate_results/intermediate_obj.obj',
+                number_of_points=600000
+            )
 
-                # load obj and sample it to point cloud
-                # set color according to its semantic type
-                part_pcd = None
-                for part_obj_file_name in os.listdir('D:/softwares/blender/Intermediate_results/'):
-                    if '_part.obj' in part_obj_file_name:
-                        part_pcd = read_one_mesh(
-                            'D:/softwares/blender/Intermediate_results/' + part_obj_file_name,
-                            number_of_points=50000)
+            # ******************* #
+            # points in parts with label
+            # ******************* #
+            # run python script in blender to generate a series of .obj files.
+            # Each .obj file contains objects of one semantic type.
+            # python script: generate_obj_with_label_using_blender.py
+            site = {
+                "source_blender": "Intermediate_results/blender_scene.blend",
+                "py_script": os.path.dirname(__file__) + '/blender_scripts/'
+                             + 'generate_obj_with_label_using_blender.py'
+            }
+            cmd_str = 'D: & ' \
+                      'cd /softwares/blender/ & ' \
+                      'blender.exe --background {source_blender} --python {py_script}'.format(**site)
+            subprocess.run(cmd_str, shell=True)
 
-                        rgb_color = [a / 255.0 for a in rgb_dic[part_obj_file_name.split('_')[1]]]
-                        part_pcd.paint_uniform_color(rgb_color)
+            # load obj and sample it to point cloud
+            # set color according to its semantic type
+            part_pcd = None
+            for part_obj_file_name in os.listdir('D:/softwares/blender/Intermediate_results/'):
+                if '_part.obj' in part_obj_file_name:
+                    part_pcd_one = read_one_mesh(
+                        'D:/softwares/blender/Intermediate_results/' + part_obj_file_name,
+                        number_of_points=50000)
 
-                        if part_pcd is None:
-                            part_pcd = part_pcd
-                        else:
-                            part_pcd += part_pcd
+                    rgb_color = [a / 255.0 for a in rgb_dic[part_obj_file_name.split('_')[1]]]
+                    part_pcd_one.paint_uniform_color(rgb_color)
 
-                # whole_model_pcd: point cloud sampled from the synthetic large motor model without label
-                # part_pcd: point cloud sampled from files.
-                #           Each of them contains objects of one semantic type.
-                # set color of points in whole_model_pcd.
-                # The color should be the same as the closest point to itself in part_pcd.
-                pcd_with_color = set_points_colors(whole_model_pcd, part_pcd)
+                    if part_pcd is None:
+                        part_pcd = part_pcd_one
+                    else:
+                        part_pcd += part_pcd_one
+            # o3d.visualization.draw_geometries([part_pcd])
 
-                # save the result: pcd_with_color
-                o3d.io.write_point_cloud(
-                    os.path.join(
-                        save_dir,
-                        root.split('/')[-1].split('\\')[0] + '_' + root.split('/')[-1].split('\\')[1] + '.pcd'),
-                    pcd_with_color)
+            # whole_model_pcd: point cloud sampled from the synthetic large motor model without label
+            # part_pcd: point cloud sampled from files.
+            #           Each of them contains objects of one semantic type.
+            # set color of points in whole_model_pcd.
+            # The color should be the same as the closest point to itself in part_pcd.
+            # pcd_with_color = set_points_colors_cuda(whole_model_pcd, part_pcd)
+            pcd_with_color = set_points_colors(whole_model_pcd, part_pcd)
+
+            # save the result: pcd_with_color
+            o3d.io.write_point_cloud(
+                os.path.join(
+                    save_dir,
+                    save_path),
+                pcd_with_color)
 
 
 if __name__ == "__main__":
-    generate_pcd_with_label('E:/datasets/agiprobot/agi_large_motor_dataset_syn/',
-                            'E:/datasets/agiprobot/large_motor_syn/labeled_pcd/')
+    generate_pcd_with_label('E:/datasets/agiprobot/large_motor_syn/raw_blender_model/',
+                            'E:/datasets/agiprobot/large_motor_syn/labeled_pcd_600million_points/')
