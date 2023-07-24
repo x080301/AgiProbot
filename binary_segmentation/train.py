095592208418e087f09474ed6f77135511a69c02
x080301 <fuhao_dsst@163.com>
2023-07-25 00:16:51 +0200

    Update train.py

--- binary_segmentation/train.py
+++ binary_segmentation/train.py
@@ -2,7 +2,7 @@ train_txt = None
 
 from train_and_test.dpp_trainer import BinarySegmentationDPP
 
-config_dir = 'config/binary_segmentation_fine_tune_5_7_100epoch.yaml'
+config_dir = 'config/binary_segmentation_pretrain_4_5_100epoch.yaml'
 print(config_dir)
 bsdpp = BinarySegmentationDPP(train_txt, config_dir)
 bsdpp.train_dpp()
