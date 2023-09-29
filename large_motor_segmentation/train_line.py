3d30d19e1e31c9b3521a13c8089c8f7174b764bf
x080301 <fuhao_dsst@163.com>
2023-09-29 02:42:25 +0200

    Update train_line.py

--- large_motor_segmentation/train_line.py
+++ large_motor_segmentation/train_line.py
@@ -2,13 +2,25 @@ import os
 
 if __name__ == "__main__":
 
-    file_list = []
-
     while True:
+        with open("/data/users/fu/trained.txt", 'r') as f:
+            x = f.readlines()
+        if len(x) == 0:
+            file_list = []
+        else:
+            file_list = x[0].split(',')
+
         executable_str = None
         for txt_file_name in sorted(os.listdir('train_line')):
             if txt_file_name not in file_list:
                 file_list.append(txt_file_name)
+
+                with open("/data/users/fu/trained.txt", 'w') as f:
+                    for i, trained_file_name in enumerate(file_list):
+                        if i != 0:
+                            f.write(',')
+                        f.write(trained_file_name)
+
                 train_txt = 'train_line' + '/' + txt_file_name
                 with open(train_txt) as f:
                     executable_str = f.read()
