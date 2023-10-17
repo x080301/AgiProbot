90d9fee7dd750d768d6139f58e7cf410e0357cbf
x080301 <fuhao_dsst@163.com>
2023-10-17 10:54:15 +0200

    Update tensorboardreader.py

--- large_motor_segmentation/utilities/tensorboardreader.py
+++ large_motor_segmentation/utilities/tensorboardreader.py
@@ -315,8 +315,8 @@ def _pipeline_valid_sample_for_cross_validation():
 
 
 def _pipeline_valid_sample_visualization():
-    train_case_dir = r'E:\datasets\agiprobot\train_Output\2023_10_15_18_07_pointnet2'
-    plt.title("pointnet++")
+    train_case_dir = r'E:\datasets\agiprobot\train_Output\2023_10_16_13_28_DGCNN'
+    plt.title("DGCNN")
 
     y_max = 0
     y_mean = 0
