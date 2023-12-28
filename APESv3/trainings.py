e14aedb1116d3e79a74ec807e5488dd51a4bf077
x080301 <fuhao_dsst@163.com>
2023-12-28 16:11:47 +0100

    traning line

--- APESv3/trainings.py
+++ APESv3/trainings.py
@@ -6,7 +6,19 @@ if __name__ == '__main__':
         with open('training_in_line.txt', 'r') as file:
             lines = file.readlines()
 
+        cmd_to_save = []
+        running_flag = True
         for single_line in lines:
-            print(single_line)
+            single_line = single_line.strip('\n')
+            if single_line[0] == '#':
+                cmd_to_save.append(single_line)
+                if '----' in single_line:
+                    running_flag = True
+            else:
+                if running_flag:
+                    
+                    os.system(single_line)
+                cmd_to_save.append('#' + single_line)
 
+        print(cmd_to_save)
         break
