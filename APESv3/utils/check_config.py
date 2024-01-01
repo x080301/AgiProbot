ba2eac8c3ee0b8baf7ac44968cdc53dc8e830f7d
x080301 <fuhao_dsst@163.com>
2024-01-01 20:51:12 +0100

    Update check_config.py

--- APESv3/utils/check_config.py
+++ APESv3/utils/check_config.py
@@ -258,7 +258,7 @@ def check_worldsize(config, mode):
         print(f'which_gpu:{config.train.ddp.which_gpu}')
         print(f'which_gpu_type{type(config.train.ddp.which_gpu)}')
 
-        #assert (isinstance(config.train.ddp.which_gpu, list))
+        assert (isinstance(config.train.ddp.which_gpu, list))
 
         num_gpus = len(config.train.ddp.which_gpu)
         config.train.ddp.nproc_this_node = num_gpus
