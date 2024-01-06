dbe71a99c5345aa9f9c810959373fda7505f4535
x080301 <fuhao_dsst@163.com>
2024-01-06 21:01:54 +0100

    Update test_modelnet.py

--- APESv3/test_modelnet.py
+++ APESv3/test_modelnet.py
@@ -204,7 +204,7 @@ def test(local_rank, config):
             vis_test_gather_dict = vis_data_gather(config, my_model, device, rank, vis_test_gather_dict)
             # print(f'size of idx{np.asarray(vis_test_gather_dict["trained"]["idx"]).shape}')
             # print(f'size of idx{np.asarray(vis_test_gather_dict["trained"]["attention_point_score"]).shape}')
-            exit(-1)
+            # exit(-1)
             torch.distributed.all_gather(pred_gather_list, preds)
             torch.distributed.all_gather(cls_label_gather_list, cls_labels)
             torch.distributed.all_gather(sample_gather_list, samples)
