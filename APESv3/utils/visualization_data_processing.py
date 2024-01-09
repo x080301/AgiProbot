40439941108135a9e007e61cc9b58cbf1a7dbb68
x080301 <fuhao_dsst@163.com>
2024-01-09 01:36:26 +0100

    Update visualization_data_processing.py

--- APESv3/utils/visualization_data_processing.py
+++ APESv3/utils/visualization_data_processing.py
@@ -273,7 +273,8 @@ def save_sampling_score(torch_tensor_to_save_batch, points: torch.Tensor, idx: l
     # torch_tensor_to_save = torch.empty(points.shape[0], points.shape[1], 0)
     reshaped_attention_score = None
     for i in range(len(idx) - 1, -1, -1):
-        
+        print(f'idx:{idx[i].shape}')
+        print(f'attention_score:{attention_score[i].shape}')
         if reshaped_attention_score is None:
             reshaped_attention_score = torch.unsqueeze(attention_score[i], dim=1)
         else:
