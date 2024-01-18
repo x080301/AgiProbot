85f296ca476b6444bb0f4fb3cc049351420e9581
x080301 <fuhao_dsst@163.com>
2024-01-18 19:05:23 +0100

    debug

--- APESv3/models/downsample.py
+++ APESv3/models/downsample.py
@@ -359,6 +359,11 @@ class DownSampleCarve(nn.Module):
             elif self.bin_mode == 'nonuniform_split_bin':
                 self.bin_conv1 = nn.Conv1d(q_in, int(self.num_bins), 1, bias=False)
                 self.bin_conv2 = nn.Conv1d(q_in + int(self.num_bins), q_out, 1, bias=False)
+            elif self.sampling_score_multiple='':  # TODO
+                self.bin_conv1 = nn.Conv1d(q_in, int(self.num_bins), 1, bias=False)
+                self.bin_conv2 = nn.Conv1d(q_in, int(self.num_bins), 1, bias=False)
+            else:
+                raise NotImplementedError
 
         # boltzmann
         self.boltzmann_enable = config_ds.boltzmann.enable[layer]
