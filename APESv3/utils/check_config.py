3a1b670440131cbfc9df945878eec972ea63e342
x080301 <fuhao_dsst@163.com>
2024-01-28 19:12:46 +0100

    Update check_config.py

--- APESv3/utils/check_config.py
+++ APESv3/utils/check_config.py
@@ -287,8 +287,8 @@ def check_worldsize(config, mode):
 
 
 def set_config_run(config, mode):
-    check_config(config)
     # config = config_gpus(config, mode)
     config = check_worldsize(config, mode)
     config = set_port(config, mode)
+    check_config(config)
     return config
