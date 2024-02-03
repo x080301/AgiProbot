d480fcbdbfa76fbcc1c3f86e82d1b9a5a56dcdc4
x080301 <fuhao_dsst@163.com>
2024-02-03 20:01:42 +0100

    Update downsample.py

--- APESv3/models/downsample.py
+++ APESv3/models/downsample.py
@@ -387,6 +387,7 @@ def calculate_num_points_to_choose_one_iteration(probability, max_num_points, nu
     probability = b / torch.sum(
         max_num_points, dim=1, keepdim=True)
     num_points_to_choose = probability * max_num_points
+    num_points_to_choose = num_points_to_choose.int()
     # num_points_to_choose = num_points_to_choose * total_points / torch.sum(num_points_to_choose, dim=1, keepdim=True)
     num_points_to_choose = torch.where(num_points_to_choose < max_num_points, num_points_to_choose, max_num_points)
 
