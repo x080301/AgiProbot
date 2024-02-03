092b14c93aee2e939081d26877b91bd136d9572f
x080301 <fuhao_dsst@163.com>
2024-02-03 21:02:41 +0100

    Update downsample.py

--- APESv3/models/downsample.py
+++ APESv3/models/downsample.py
@@ -365,6 +365,7 @@ def calculate_num_points_to_choose(probability, max_num_points, total_points_to_
 
         num_points_to_choose += calculate_num_points_to_choose_one_iteration(probability, num_poins_to_drop,
                                                                              num_undecided_points)
+        print(f'num_points_to_choose{torch.sum(num_points_to_choose,dim=1)}')
 
     return num_points_to_choose
 
@@ -382,16 +383,16 @@ def calculate_num_points_to_choose_one_iteration(probability, max_num_points, nu
     probability = probability / torch.sum(probability, dim=1, keepdim=True) * num_undecided_points / torch.sum(
         max_num_points, dim=1, keepdim=True)
 
-    print(f'max_num_points{max_num_points}')
-    print(f'probability{probability}')
+    # print(f'max_num_points{max_num_points}')
+    # print(f'probability{probability}')
     num_points_to_choose = probability * max_num_points
-    print(f'num_points_to_choose2:{num_points_to_choose}')
+    # print(f'num_points_to_choose2:{num_points_to_choose}')
     num_points_to_choose = num_points_to_choose.int()
 
-    print(f'num_points_to_choose1:{num_points_to_choose}')
+    # print(f'num_points_to_choose1:{num_points_to_choose}')
     # num_points_to_choose = num_points_to_choose * total_points / torch.sum(num_points_to_choose, dim=1, keepdim=True)
     num_points_to_choose = torch.where(num_points_to_choose < max_num_points, num_points_to_choose, max_num_points)
-    print(f'num_points_to_choose:{num_points_to_choose}')
+    # print(f'num_points_to_choose:{num_points_to_choose}')
 
     return num_points_to_choose
 
