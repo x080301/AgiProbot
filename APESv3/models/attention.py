a01705212a90bf9934161beed5f09d55b0b9f8ba
x080301 <fuhao_dsst@163.com>
2024-02-04 00:36:05 +0100

    Update attention.py

--- APESv3/models/attention.py
+++ APESv3/models/attention.py
@@ -3,6 +3,113 @@ from utils import ops
 import math
 import torch
 
+class L2Attention(nn.Module):
+    def __init__(self, config_attention, layer):
+        super(Neighbor2PointAttention, self).__init__()
+        self.K = config_attention.K[layer]
+        self.group_type = config_attention.group_type[layer]
+        self.num_heads = config_attention.num_heads[layer]
+        self.attention_mode = config_attention.attention_mode[layer]
+        q_in = config_attention.q_in[layer]
+        q_out = config_attention.q_out[layer]
+        k_in = config_attention.k_in[layer]
+        k_out = config_attention.k_out[layer]
+        v_in = config_attention.v_in[layer]
+        v_out = config_attention.v_out[layer]
+        ff_conv1_channels_in = config_attention.ff_conv1_channels_in[layer]
+        ff_conv1_channels_out = config_attention.ff_conv1_channels_out[layer]
+        ff_conv2_channels_in = config_attention.ff_conv2_channels_in[layer]
+        ff_conv2_channels_out = config_attention.ff_conv2_channels_out[layer]
+        self.asm = config_attention.asm[layer]
+
+        self.q_depth = int(q_out / self.num_heads)
+        self.k_depth = int(k_out / self.num_heads)
+        self.v_depth = int(v_out / self.num_heads)
+
+        self.q_conv = nn.Conv2d(q_in, q_out, 1, bias=False)
+        self.k_conv = nn.Conv2d(k_in, k_out, 1, bias=False)
+        self.v_conv = nn.Conv2d(v_in, v_out, 1, bias=False)
+        self.softmax = nn.Softmax(dim=-1)
+        self.ff = nn.Sequential(nn.Conv1d(ff_conv1_channels_in, ff_conv1_channels_out, 1, bias=False),
+                                nn.LeakyReLU(negative_slope=0.2),
+                                nn.Conv1d(ff_conv2_channels_in, ff_conv2_channels_out, 1, bias=False))
+        self.bn1 = nn.BatchNorm1d(v_out)
+        self.bn2 = nn.BatchNorm1d(v_out)
+
+    def forward(self, x):
+        # x.shape == (B, C, N)
+        neighbors, _ = ops.group(x, self.K, self.group_type)
+        # neighbors.shape == (B, C, N, K)
+        x_tmp = x[:, :, :, None]
+        # x_tmp.shape == (B, C, N, 1)
+        q = self.q_conv(x_tmp)
+        # q.shape == (B, C, N, 1)
+        q = self.split_heads(q, self.num_heads, self.q_depth)
+        # q.shape == (B, H, N, 1, D)
+        k = self.k_conv(neighbors)
+        # k.shape ==  (B, C, N, K)
+        k = self.split_heads(k, self.num_heads, self.k_depth)
+        # k.shape == (B, H, N, K, D)
+        v = self.v_conv(neighbors)
+        # v.shape ==  (B, C, N, K)
+        v = self.split_heads(v, self.num_heads, self.v_depth)
+        # v.shape == (B, H, N, K, D)
+        k = k.permute(0, 1, 2, 4, 3)
+        # k.shape == (B, H, N, D, K)
+        x_tmp = self.attention(q, k, v)
+        # x_tmp.shape == (B, C, N)
+        x = self.bn1(x + x_tmp)
+        # x.shape == (B, C, N)
+        x_tmp = self.ff(x)
+        # x_tmp.shape == (B, C, N)
+        x = self.bn2(x + x_tmp)
+        # x.shape == (B, C, N)
+        return x
+
+    def split_heads(self, x, heads, depth):
+        # x.shape == (B, C, N, K)
+        x = x.view(x.shape[0], heads, depth, x.shape[2], x.shape[3])
+        # x.shape == (B, H, D, N, K)
+        x = x.permute(0, 1, 3, 4, 2)
+        # x.shape == (B, H, N, K, D)
+        return x
+
+    def attention(self, q, k, v):
+        """
+        Local based attention variation input shape:
+        q.shape == (B, H, N, 1, D)
+        k.shape == (B, H, N, D, K)
+        v.shape == (B, H, N, K, D)
+        """
+        if self.attention_mode == "scalar_dot":
+            attention = self.attention_scoring(q, k)  # attention.shape == (B, H, N, 1, K)
+            x = (attention @ v)[:, :, :, 0, :].permute(0, 2, 1, 3)
+            # x.shape == (B, N, H, D)
+        elif self.attention_mode == "vector_sub":
+            energy = q.repeat(1, 1, 1, k.shape[-1], 1) - k.permute(0, 1, 2, 4, 3)
+            # energy.shape == (B, H, N, K, D)
+            scale_factor = math.sqrt(q.shape[-1])
+            attention = self.softmax(energy / scale_factor)
+            # attention.shape == (B, H, N, K, D)
+            x = (attention * v).permute(0, 2, 1, 3, 4)
+            # x.shape == (B, N, H, K, D) element-wise multiplication
+            x = x.sum(dim=-2)
+            # x.shape == (B, N, H, D)
+        else:
+            raise ValueError('attention_mode can only be scalar_dot or vector_sub, but got: {self.attention_mode}')
+        x = x.reshape(x.shape[0], x.shape[1], -1).permute(0, 2, 1)  # x_tmp.shape == (B, C, N)
+        return x
+
+    def attention_scoring(self, q, k):  # q.shape == B, H, N, 1, D), k.shape == (B, H, N, D, K)
+        if self.asm == "dot":
+            energy = q @ k  # energy.shape == (B, H, N, 1, K)
+        elif self.asm == "dot-sub":
+            energy = q @ (q.transpose(-1, -2) - k)  # Q@(Q-K)
+        else:
+            raise ValueError('Please check the setting of asm in feature learning layer!')
+        scale_factor = math.sqrt(q.shape[-1])
+        attention = self.softmax(energy / scale_factor)  # attention.shape == (B, H, N, 1, K)
+        return attention
 
 class Neighbor2PointAttention(nn.Module):
     def __init__(self, config_attention, layer):
