7bb9f5d8f5d116bb1833d207a3aaf3a283e41747
x080301 <fuhao_dsst@163.com>
2024-02-07 00:23:11 +0100

    Update downsample.py

--- APESv3/models/downsample.py
+++ APESv3/models/downsample.py
@@ -315,7 +315,7 @@ def bin_probability_multiple(x_ds, input_x_shape, down_sampling_idx, bin_chunks_
     bin_probability = bin_probability / torch.sum(bin_probability, dim=1, keepdim=True)
     assert down_sampling_idx.shape[1] == 1, "Number of heads should be 1!"
 
-    tensor_to_multiply = torch.zeros(B, N).to(bin_probability.device) + torch.sum(bin_probability)
+    tensor_to_multiply = torch.zeros(B, M).to(bin_probability.device) + torch.sum(bin_probability)
 
     # for i in range(num_bins):
     #     for j in range(B):
@@ -334,7 +334,7 @@ def bin_probability_multiple(x_ds, input_x_shape, down_sampling_idx, bin_chunks_
     #         #                                                             bin_chunks_idx[i].squeeze()[j, :],
     #         #                                                             1.0 + bin_probability[j, i] / M)
 
-    tensor_to_multiply = torch.gather(tensor_to_multiply, dim=1, index=down_sampling_idx.squeeze(dim=1)).unsqueeze(1)
+    # tensor_to_multiply = torch.gather(tensor_to_multiply, dim=1, index=down_sampling_idx.squeeze(dim=1)).unsqueeze(1)
 
     # print(f'x_ds.shape=={x_ds.shape}')
     # print(f'tensor_to_multiply:{tensor_to_multiply.shape}')
