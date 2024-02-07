cc9b57e320e2871938c144188638280b1d875a2b
x080301 <fuhao_dsst@163.com>
2024-02-08 00:03:40 +0100

    Update test_modelnet.py

--- APESv3/test_modelnet.py
+++ APESv3/test_modelnet.py
@@ -254,6 +254,7 @@ def test(local_rank, config):
                     # idx_down_all_layers: num_layers * (B,H,M) -> (B, num_layers, H, N)
                     idx_down = reshape_gathered_variable(idx_down_all_layers)
                     # idx_in_bins_all_layers: num_layers * (B,num_bins,1,n) or num_layers * B * num_bins * (1,n) -> (B, num_layers, num_bins, H, n) or B * num_layers * num_bins * (H,n)
+                    print(f'type(idx_in_bins_all_layers[0][0][0]){type(idx_in_bins_all_layers[0][0][0])}')
                     idx_in_bins = reshape_gathered_variable(idx_in_bins_all_layers)
                     print(f'type(idx_in_bins[0][0][0]{type(idx_in_bins[0][0][0])}')
                     # probability_of_bins_all_layers: num_layers * (B, num_bins) -> (B, num_layers, num_bins)
