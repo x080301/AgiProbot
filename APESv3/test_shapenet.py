221760ef373ab02ab070b9f5f6859e5e249350e6
x080301 <fuhao_dsst@163.com>
2024-02-13 01:05:04 +0100

    Update test_shapenet.py

--- APESv3/test_shapenet.py
+++ APESv3/test_shapenet.py
@@ -300,14 +300,14 @@ def test(local_rank, config):
                                 pickle.dump(data_dict, f)
                             # print(f'save{i}')
 
-                        visualization_heatmap(mode='shapenet', data_dict=data_dict,
-                                              save_path=f'{save_dir}heat_map', index=i)
-                        visualization_downsampled_points(mode='shapenet', data_dict=data_dict,
-                                                         save_path=f'{save_dir}downsampled_points', index=i)
-                        visualization_points_in_bins(mode='shapenet', data_dict=data_dict,
-                                                     save_path=f'{save_dir}points_in_bins', index=i)
-                        visualization_histogram(mode='shapenet', data_dict=data_dict,
-                                                save_path=f'{save_dir}histogram', index=i)
+                        # visualization_heatmap(mode='shapenet', data_dict=data_dict,
+                        #                       save_path=f'{save_dir}heat_map', index=i)
+                        # visualization_downsampled_points(mode='shapenet', data_dict=data_dict,
+                        #                                  save_path=f'{save_dir}downsampled_points', index=i)
+                        # visualization_points_in_bins(mode='shapenet', data_dict=data_dict,
+                        #                              save_path=f'{save_dir}points_in_bins', index=i)
+                        # visualization_histogram(mode='shapenet', data_dict=data_dict,
+                        #                         save_path=f'{save_dir}histogram', index=i)
 
             if rank == 0:
                 preds = torch.concat(pred_gather_list, dim=0)
