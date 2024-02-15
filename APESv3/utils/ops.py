308083dbd383544c2d27ad4dc15cea568bb8d524
x080301 <fuhao_dsst@163.com>
2024-02-15 22:51:32 +0100

    Update ops.py

--- APESv3/utils/ops.py
+++ APESv3/utils/ops.py
@@ -138,9 +138,10 @@ def update_sampling_score_bin_boundary(old_bin_boundaries, attention_point_score
     bin_boundaries = bin_boundaries / torch.distributed.get_world_size()
 
     if old_bin_boundaries is not None:
-        bin_boundaries = old_bin_boundaries[0].detach()[0, 0, 0, 1:] * 0.99 + 0.01 * bin_boundaries
+        new_bin_boundaries = old_bin_boundaries.detach()
 
-        new_bin_boundaries = old_bin_boundaries
+        bin_boundaries = new_bin_boundaries[0][0, 0, 0, 1:] * 0.99 + 0.01 * bin_boundaries
+
         new_bin_boundaries[0][0, 0, 0, 1:] = bin_boundaries
         new_bin_boundaries[1][0, 0, 0, :-1] = bin_boundaries
     else:
