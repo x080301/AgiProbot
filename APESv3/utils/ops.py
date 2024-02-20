f0ad808f06805a904b19f3c22f6aa5637a63fb56
x080301 <fuhao_dsst@163.com>
2024-02-20 23:21:16 +0100

    Update ops.py

--- APESv3/utils/ops.py
+++ APESv3/utils/ops.py
@@ -191,7 +191,7 @@ def sort_chunk_nonuniform(attention_point_score, bin_boundaries, num_bins, norma
             print(f'attention_point_score:\n{attention_point_score}')
             print(f'zero or negative value exists = {torch.min(attention_point_score).item() <= 0}')
             print(f'minimun is {torch.min(attention_point_score).item()}')
-            
+
         attention_point_score = attention_point_score - torch.mean(attention_point_score, dim=2, keepdim=True)
 
     attention_point_score = attention_point_score.reshape(B, H, N, 1)
