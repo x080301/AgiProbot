5a57bba9cb37363373c641cfdcdd3dd09d643e71
x080301 <fuhao_dsst@163.com>
2024-02-21 17:05:40 +0100

    Update downsample.py

--- APESv3/models/downsample.py
+++ APESv3/models/downsample.py
@@ -234,6 +234,7 @@ def nonuniform_bin_idx_selection_beforesoftmaxbinprob(attention_point_score, bin
 
     # print('-------------------------')
     # print(f'shape:{masked_attention_map_token.shape}')
+    print(f'masked_attention_map_token[0,0,:,0]:{masked_attention_map_token[0,0,:,0]}')
     negative_num = torch.sum(masked_attention_map_token < 0, dim=2).squeeze(1)
     print(f'\nnegative numbers in bin tokens of layer {int(M / 512)}:\n{negative_num}')
     # # print(f'k_point_to_choose{torch.sum(k_point_to_choose,dim=1)}')
