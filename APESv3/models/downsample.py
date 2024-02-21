327ad94b4ba9ac6b460513008deb9d0a164cc315
x080301 <fuhao_dsst@163.com>
2024-02-21 16:57:09 +0100

    Update downsample.py

--- APESv3/models/downsample.py
+++ APESv3/models/downsample.py
@@ -232,12 +232,13 @@ def nonuniform_bin_idx_selection_beforesoftmaxbinprob(attention_point_score, bin
     k_point_to_choose = calculate_num_points_to_choose(bin_prob, max_num_points, M)
     # k_point_to_choose.shape == (B, num_bins)
 
-    print('-------------------------')
-    print(f'shape:{masked_attention_map_token.shape}')
-    negative_num = torch.count_nonzero(masked_attention_map_token < 0, dim=2).squeeze(1)
-    print(f'\nnegative numbers in bin tokens of layer {int(M / 512)}:\n{negative_num}')
-    # print(f'k_point_to_choose{torch.sum(k_point_to_choose,dim=1)}')
-    print(f'k_point_to_choose:{k_point_to_choose}')
+    # print('-------------------------')
+    print(f'masked_attention_map_token{masked_attention_map_token}')
+    # print(f'shape:{masked_attention_map_token.shape}')
+    # negative_num = torch.count_nonzero(masked_attention_map_token < 0, dim=2).squeeze(1)
+    # print(f'\nnegative numbers in bin tokens of layer {int(M / 512)}:\n{negative_num}')
+    # # print(f'k_point_to_choose{torch.sum(k_point_to_choose,dim=1)}')
+    # print(f'k_point_to_choose:{k_point_to_choose}')
 
     idx_batch_list = []
     for i in range(B):
