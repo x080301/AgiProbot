8bdb6cfbe4f957a9b2047bd8f4afb2a731d49519
x080301 <fuhao_dsst@163.com>
2024-03-27 09:58:03 +0100

    Update ops.py

--- APESv3/utils/ops.py
+++ APESv3/utils/ops.py
@@ -547,6 +547,10 @@ def generating_downsampled_index(M, attention_point_score, bin_points_mask, bin_
             # attention_point_score: (B, H, N)
             # bin_points_mask: (B, H, N, num_bins)
 
+            print(f'number of nan:{torch.sum(torch.isnan(attention_point_score))}')
+            print(f'number of inf:{torch.sum(torch.isinf(attention_point_score))}')
+            print(f'number of negative:{torch.sum(attention_point_score < 0)}')
+
             sampling_probabilities = torch.exp(attention_point_score.unsqueeze(3) / boltzmann_T) * bin_points_mask
             sampling_probabilities = sampling_probabilities / torch.sum(sampling_probabilities, dim=2, keepdim=True)
             sampling_probabilities = sampling_probabilities.squeeze(dim=1)
@@ -556,9 +560,7 @@ def generating_downsampled_index(M, attention_point_score, bin_points_mask, bin_
 
         sampling_probabilities = sampling_probabilities.permute(0, 2, 1).reshape(-1, N)
         # sampling_probabilities: (B*num_bins,N)
-        print(f'number of nan:{torch.sum(torch.isnan(sampling_probabilities))}')
-        print(f'number of inf:{torch.sum(torch.isinf(sampling_probabilities))}')
-        print(f'number of negative:{torch.sum(sampling_probabilities < 0)}')
+
         sampled_index_M_points = torch.multinomial(sampling_probabilities, M)
         # sampled_index_M_points: (B*num_bins,M)
         sampled_index_M_points = sampled_index_M_points.reshape(B, num_bins, M)
