78f7adc78f960e5fb5dec449e5565fdb453058bb
x080301 <fuhao_dsst@163.com>
2024-04-10 10:57:02 +0200

    new test

--- APESv3/generate_test_cmd.py
+++ APESv3/generate_test_cmd.py
@@ -19,17 +19,21 @@ def get_trained_runs():
 
 
 test_list = [
-    "python train_modelnet.py train.epochs=200 train.ddp.which_gpu=[] datasets=modelnet_AnTao420M usr_config=configs/boltzmannT01.yaml wandb.name='Modelnet_Token_Std_boltzmann_T01_norm_sparsesum1_1'",
-    "python train_modelnet.py train.epochs=200 train.ddp.which_gpu=[] datasets=modelnet_AnTao420M usr_config=configs/boltzmannT01.yaml wandb.name='Modelnet_Token_Std_boltzmann_T01_norm_sparsesum1_2'",
-    "python train_modelnet.py train.epochs=200 train.ddp.which_gpu=[] datasets=modelnet_AnTao420M usr_config=configs/boltzmannT01.yaml wandb.name='Modelnet_Token_Std_boltzmann_T01_norm_sparsesum1_3'",
-    "python train_modelnet.py train.epochs=200 train.ddp.which_gpu=[] datasets=modelnet_AnTao420M usr_config=configs/boltzmannT01.yaml wandb.name='Modelnet_Token_Std_boltzmann_T01_norm_sparsesum1_4'",
-    "python train_modelnet.py train.epochs=200 train.ddp.which_gpu=[] datasets=modelnet_AnTao420M usr_config=configs/boltzmannT01.yaml wandb.name='Modelnet_Token_Std_boltzmann_T01_norm_sparsesum1_5'",
-    "python train_modelnet.py train.epochs=200 train.ddp.which_gpu=[] datasets=modelnet_AnTao420M usr_config=configs/boltzmannT01.yaml wandb.name='Modelnet_Token_Std_boltzmann_T01_norm_sparsesum1_6'",
-    "python train_modelnet.py train.epochs=200 train.ddp.which_gpu=[] datasets=modelnet_AnTao420M usr_config=configs/boltzmannT01.yaml wandb.name='Modelnet_Token_Std_boltzmann_T01_norm_sparsesum1_7'",
-    "python train_modelnet.py train.epochs=200 train.ddp.which_gpu=[] datasets=modelnet_AnTao420M usr_config=configs/boltzmannT01.yaml wandb.name='Modelnet_Token_Std_boltzmann_T01_norm_sparsesum1_8'",
-    "python train_modelnet.py train.epochs=200 train.ddp.which_gpu=[] datasets=modelnet_AnTao420M usr_config=configs/boltzmannT01.yaml wandb.name='Modelnet_Token_Std_boltzmann_T01_norm_sparsesum1_9'",
-    "python train_modelnet.py train.epochs=200 train.ddp.which_gpu=[] datasets=modelnet_AnTao420M usr_config=configs/boltzmannT01.yaml wandb.name='Modelnet_Token_Std_boltzmann_T01_norm_sparsesum1_10'"]
 
+    "python train_modelnet.py train.epochs=200 train.ddp.which_gpu=[] datasets=modelnet_AnTao420M usr_config=configs/boltzmannT0102.yaml wandb.name='Modelnet_Token_Std_boltzmann_T0102_norm_sparsesum1_1'",
+    "python train_modelnet.py train.epochs=200 train.ddp.which_gpu=[] datasets=modelnet_AnTao420M usr_config=configs/boltzmannT0102.yaml wandb.name='Modelnet_Token_Std_boltzmann_T0102_norm_sparsesum1_2'",
+    "python train_modelnet.py train.epochs=200 train.ddp.which_gpu=[] datasets=modelnet_AnTao420M usr_config=configs/boltzmannT0102.yaml wandb.name='Modelnet_Token_Std_boltzmann_T0102_norm_sparsesum1_3'",
+    "python train_modelnet.py train.epochs=200 train.ddp.which_gpu=[] datasets=modelnet_AnTao420M usr_config=configs/boltzmannT0102.yaml wandb.name='Modelnet_Token_Std_boltzmann_T0102_norm_sparsesum1_4'",
+    "python train_modelnet.py train.epochs=200 train.ddp.which_gpu=[] datasets=modelnet_AnTao420M usr_config=configs/boltzmannT0102.yaml wandb.name='Modelnet_Token_Std_boltzmann_T0102_norm_sparsesum1_5'",
+    "python train_modelnet.py train.epochs=200 train.ddp.which_gpu=[] datasets=modelnet_AnTao420M usr_config=configs/boltzmannT0102.yaml wandb.name='Modelnet_Token_Std_boltzmann_T0102_norm_sparsesum1_6'",
+    "python train_modelnet.py train.epochs=200 train.ddp.which_gpu=[] datasets=modelnet_AnTao420M usr_config=configs/boltzmannT0102.yaml wandb.name='Modelnet_Token_Std_boltzmann_T0102_norm_sparsesum1_7'",
+    "python train_modelnet.py train.epochs=200 train.ddp.which_gpu=[] datasets=modelnet_AnTao420M usr_config=configs/boltzmannT0102.yaml wandb.name='Modelnet_Token_Std_boltzmann_T0102_norm_sparsesum1_8'",
+    "python train_modelnet.py train.epochs=200 train.ddp.which_gpu=[] datasets=modelnet_AnTao420M usr_config=configs/boltzmannT0102.yaml wandb.name='Modelnet_Token_Std_boltzmann_T0102_norm_sparsesum1_9'",
+    "python train_modelnet.py train.epochs=200 train.ddp.which_gpu=[] datasets=modelnet_AnTao420M usr_config=configs/boltzmannT0102.yaml wandb.name='Modelnet_Token_Std_boltzmann_T0102_norm_sparsesum1_10'"
+
+
+
+]
 trained_list = get_trained_runs()
 
 subprocess.run('nvidia-smi', shell=True, text=True, stdout=None, stderr=subprocess.PIPE)
