8bbdc4993053bd6d6bcae37f89a11f9187f7df6c
x080301 <fuhao_dsst@163.com>
2024-04-10 19:06:38 +0200

    new knn

--- APESv3/utils/ops.py
+++ APESv3/utils/ops.py
@@ -20,12 +20,19 @@ def knn(a, b, k):
     :param b: b.shape == (B, M, C)
     :param k: int
     """
+    a_mean = torch.mean(a, dim=1, keepdim=True)
+    a = a - a_mean
+    b = b - a_mean
+    a_std = torch.mean(torch.std(a, dim=1, keepdim=True), dim=2, keepdim=True)
+    a = a / a_std
+    b = b / a_std
+
     # inner = -2 * torch.matmul(a, b.transpose(2, 1))  # inner.shape == (B, N, M)
     # aa = torch.sum(a ** 2, dim=2, keepdim=True)  # aa.shape == (B, N, 1)
     # bb = torch.sum(b ** 2, dim=2, keepdim=True)  # bb.shape == (B, M, 1)
     # # TODO: some values inside pairwise_distance is positive
     # pairwise_distance = -aa - inner - bb.transpose(2, 1)  # pairwise_distance.shape == (B, N, M)
-    pairwise_distance = -torch.cdist(a, b, compute_mode='donot_use_mm_for_euclid_dist')
+    pairwise_distance = -torch.cdist(a, b)  # , compute_mode='donot_use_mm_for_euclid_dist')
 
     # num_positive = torch.sum(pairwise_distance > 0)
 
