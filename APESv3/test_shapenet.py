da8811a416e34e983aa24f81517239e8b09f066d
x080301 <fuhao_dsst@163.com>
2024-05-30 01:23:33 +0200

    Update test_shapenet.py

--- APESv3/test_shapenet.py
+++ APESv3/test_shapenet.py
@@ -398,8 +398,10 @@ def test(local_rank, config):
                 loss /= config.test.ddp.nproc_this_node
                 loss_list.append(loss.detach().cpu().numpy())
                 pbar.update(i)
-                if config.test.save_pkl:
-                    save_statical_data(data_dict, save_dir, statistic_data_all_samples)
+            break
+        if rank == 0:
+            if config.test.save_pkl:
+                save_statical_data(data_dict, save_dir, statistic_data_all_samples)
 
 
 if __name__ == '__main__':
