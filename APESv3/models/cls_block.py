22f5a3a2a0c37fd2d6f9ad058274f5f16ebf685b
x080301 <fuhao_dsst@163.com>
2025-01-29 21:43:20 +0100

    Update cls_block.py

--- APESv3/models/cls_block.py
+++ APESv3/models/cls_block.py
@@ -145,7 +145,7 @@ class FeatureLearningBlock(nn.Module):
                 # x.shape, torch.Size([8, 128, 2048])
                 if self.fps:
                     x_idx = farthest_point_sample(torch.permute(x_xyz, (0, 2, 1)), self.M_list[i] * 2)  # [B, npoint]
-                    x = torch.gather(x, x_idx.unsqueeze(1).expand(-1, 3, -1))
+                    x = torch.gather(x, 2, x_idx.unsqueeze(1).expand(-1, 3, -1))
                     x_xyz = torch.gather(x_xyz, 2, x_idx.unsqueeze(1).expand(-1, 3, -1))
 
                 (x, idx_select) = self.downsample_list[i](x, x_xyz)[0]
