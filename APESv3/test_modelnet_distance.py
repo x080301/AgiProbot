d70e71ec05dbdf6a4f8afad8ee650342b1f40578
x080301 <fuhao_dsst@163.com>
2025-01-29 13:32:48 +0100

    Update test_modelnet_distance.py

--- APESv3/test_modelnet_distance.py
+++ APESv3/test_modelnet_distance.py
@@ -189,7 +189,8 @@ def test(local_rank, config):
     my_model = torch.nn.parallel.DistributedDataParallel(my_model)
     map_location = {'cuda:0': f'cuda:{local_rank}'}
 
-    print(f'config.feature_learning_block.downsample.bin.dynamic_boundaries={config.feature_learning_block.downsample.bin.dynamic_boundaries}')
+    print(
+        f'config.feature_learning_block.downsample.bin.dynamic_boundaries={config.feature_learning_block.downsample.bin.dynamic_boundaries}')
     config.feature_learning_block.downsample.bin.dynamic_boundaries = False
     if config.feature_learning_block.downsample.bin.dynamic_boundaries:
         state_dict = torch.load(f'{artifacts_path}/checkpoint.pt', map_location=map_location)
@@ -200,7 +201,10 @@ def test(local_rank, config):
             bin_boundaries[0][0, 0, 0, 1:].tolist()
             for bin_boundaries in state_dict['bin_boundaries']]
     else:
-        my_model.load_state_dict(torch.load(f'{artifacts_path}/checkpoint.pt', map_location=map_location))
+        # my_model.load_state_dict(torch.load(f'{artifacts_path}/checkpoint.pt', map_location=map_location))
+
+        state_dict = torch.load(f'{artifacts_path}/checkpoint.pt', map_location=map_location)
+        my_model.load_state_dict(state_dict['model_state_dict'])
 
     # get loss function
     if config.test.label_smoothing:
